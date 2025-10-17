696d02922b12bb4f562821ed39b334cd5df75035
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-10-16 20:10:48 -0600

    Create 4-bit binary counter using the 4 LED's

--- app/src/main.c
+++ app/src/main.c
@@ -17,10 +17,15 @@ int main(void) {
         return 0;
     }
 
+    int counter = 0;
     while(1) {
         if(BTN_check_clear_pressed(BTN0)) {
-            LED_toggle(LED0);
+            LED_set(LED0, (counter >> 0) & 0x1);
+            LED_set(LED1, (counter >> 1) & 0x1);
+            LED_set(LED2, (counter >> 2) & 0x1);
+            LED_set(LED3, (counter >> 3) & 0x1);
             printk("Button 0 pressed!\n");
+            counter = (counter + 1) % 16;
         }
         k_msleep(SLEEP_MS);
     }
