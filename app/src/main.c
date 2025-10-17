5ef680edf90333580dbcace4f0a8805acd9037cd
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-10-16 19:39:55 -0600

    Setup button and picocom serial monitor

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,29 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
+#define SLEEP_TIME_MS 1000
+
+#define SW0_NODE DT_ALIAS(sw0)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
 int main(void) {
+    int ret;
 
-  while(1) {
+    if(!gpio_is_ready_dt(&button)) {
+        return 0;
+    }
 
-  }
-	return 0;
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if(0 > ret) {
+        return 0;
+    }
+
+    while(1) {
+        ret = gpio_pin_get_dt(&button);
+        if(0 < ret) {
+            printk("Pressed!\n");
+        }
+    }
+    k_msleep(SLEEP_TIME_MS);
+    return 0;
 }
