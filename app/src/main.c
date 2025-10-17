e50b822231247eb27a9b98eb9aa042ba553218c5
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-10-16 20:00:55 -0600

    Fix button bounce issue using the API provided

--- app/src/main.c
+++ app/src/main.c
@@ -3,43 +3,26 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
-#include <inttypes.h>
 
-#define SLEEP_TIME_MS 1000
+#include "BTN.h"
+#include "LED.h"
 
-#define SW0_NODE DT_ALIAS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
-static struct gpio_callback button_isr_data;
-
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
-    printk("Button 0 pressed!\n");
-}
+#define SLEEP_MS 1
 
 int main(void) {
-    int ret;
-
-    if(!gpio_is_ready_dt(&button)) {
+    if(0 > BTN_init()) {
         return 0;
     }
-
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if(0 > ret) {
+    if(0 > LED_init()) {
         return 0;
     }
-
-    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    if(0 > ret) {
-        return 0;
-    }
-
-    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-    gpio_add_callback(button.port, &button_isr_data);
 
     while(1) {
+        if(BTN_check_clear_pressed(BTN0)) {
+            LED_toggle(LED0);
+            printk("Button 0 pressed!\n");
+        }
+        k_msleep(SLEEP_MS);
     }
     return 0;
 }
