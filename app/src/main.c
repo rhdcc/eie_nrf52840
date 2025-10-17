476a66551065a4975b0fd3a5d861806fd65cbaf5
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-10-16 19:49:39 -0600

    Implement basic interreput driven output

--- app/src/main.c
+++ app/src/main.c
@@ -13,6 +13,12 @@
 #define SW0_NODE DT_ALIAS(sw0)
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
+static struct gpio_callback button_isr_data;
+
+void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+    printk("Button 0 pressed!\n");
+}
+
 int main(void) {
     int ret;
 
@@ -25,12 +31,15 @@ int main(void) {
         return 0;
     }
 
-    while(1) {
-        ret = gpio_pin_get_dt(&button);
-        if(0 < ret) {
-            printk("Pressed!\n");
-        }
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if(0 > ret) {
+        return 0;
     }
-    k_msleep(SLEEP_TIME_MS);
+
+    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
+    gpio_add_callback(button.port, &button_isr_data);
+
+    while(1) {
+    }
     return 0;
 }
