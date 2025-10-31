bdfbd5e37da54d9a37064de51124738720de0d62
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-10-30 19:05:42 -0600

    Create example Pulse Width Modulation code
    Create example code taken directly from the lesson 6 README.

---
+++ app/src/example_program.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ */
+
+#include <inttypes.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#include "BTN.h"
+#include "LED.h"
+
+#define SLEEP_MS 1
+
+int main(void) {
+    if (0 > BTN_init()) {
+        return 0;
+    }
+    if (0 > LED_init()) {
+        return 0;
+    }
+
+    uint8_t current_duty_cycle = 0; // as a percentage
+
+    LED_pwm(LED0, current_duty_cycle);
+
+    while(1) {
+        if(BTN_check_clear_pressed(BTN0)) {
+            current_duty_cycle = (current_duty_cycle >= 100) ? 0 : (current_duty_cycle + 10);
+            printk("Setting LED0 to %d%% brightness.\n", current_duty_cycle);
+            LED_pwm(LED0, current_duty_cycle);
+        }
+        k_msleep(SLEEP_MS);
+    }
+    return 0;
+}
